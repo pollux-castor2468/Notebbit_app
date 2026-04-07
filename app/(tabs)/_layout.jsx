15b2ba3f3d086898913a886bb245dd1916be6e45
張依蓁 <a0966150665@gmail.com>
2026-04-07 22:57:25 +0800

    add home icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,12 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="(home)" options={{title: 'HOME'}} />
+            <Tabs.Screen name="(home)" options={{
+                title: 'HOME', headerShown: false,
+                tabBarIcon: ({focused}) => (<Ionicons name="home" size={20} color={focused ? 'black' : 'gray'} />)}} />
             <Tabs.Screen name="setting" options={{title: 'setting'}} />
         </Tabs>
     )
