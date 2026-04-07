f10c78045292f9208636e59d8ab4eb5bf06fb783
張依蓁 <a0966150665@gmail.com>
2026-04-07 22:29:56 +0800

    first commit

---
+++ app/(tabs)/(home)/index.jsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Notes() {
+    return (
+        <View>
+            <Text>home page</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+})
---
+++ app/(tabs)/setting.jsx
@@ -0,0 +1,9 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Setting() {
+    return (
+        <View>
+            <Text>setting page</Text>
+        </View>
+    );
+}
